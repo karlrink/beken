e915b751c43216777ef7fd40e36f81a5531b276d
Karl Rink <karl@rink.us>
2023-08-26 15:47:42 -0700

    🐕-2023-08-26 🐕 🐕

--- touer/main.go
+++ touer/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -21,21 +23,28 @@ func usage() string {
 Options:
 
   list-tables
-  list-ips
-  list-tokens
-  list-procs
 
   proc-ips
 
+  list-procs
+  del-proc n
+  purge-procs
+
+  list-ips
   add-ip ip data
   del-ip ip
+  purge-ips
 
+  list-tokens
   add-token token data
   del-token token
+  purge-tokens
 
   list-configs
   add-config name json
   del-config name
+  purge-configs
+
 
 `
 	return usage
@@ -44,21 +53,19 @@ Options:
 func main() {
 
 	if len(os.Args) < 2 {
-		fmt.Println(usage())
-		return
+		printErr(usage())
 	}
 
 	switch os.Args[1] {
 	case "--help", "-help", "help":
-		fmt.Println(usage())
-		return
+		printErr(usage())
 	case "--version", "-version", "version":
-		fmt.Println("Version:", version)
+		printOut("Version: " + version)
 		sqlite3version, err := getSqlite3Version(os.Args[1])
 		if err != nil {
-			log.Fatalf("Failed to get SQLite version: %v", err)
+			printErr("Failed to get SQLite version: %v", err)
 		}
-		fmt.Println("Sqlite3:", sqlite3version)
+		printOut("Sqlite3: " + sqlite3version)
 		return
 	}
 
@@ -66,24 +73,18 @@ func main() {
 
 	// Check if the file exists
 	if _, err := os.Stat(dbFile); os.IsNotExist(err) {
-		//log.Fatalf("File not found: %s", dbFile)
-		fmt.Fprintf(os.Stderr, "File not found: %s\n", dbFile)
-		return
+		printErr("File not found: %s\n", dbFile)
 	}
 	// Connect to the SQLite3 database file
 	db, err := sql.Open("sqlite3", dbFile)
 	if err != nil {
-		//log.Fatalf("Failed to connect: %v", err)
-		//fmt.Fprintf("Failed to connect: %s", err)
-		fmt.Printf("Failed to connect: %s\n", err)
-		return
+		printErr("Failed to connect: %s\n", err)
 	}
 	defer db.Close()
 
 	// Check if the database connection is valid by pinging it
 	if err := db.Ping(); err != nil {
-		fmt.Printf("Failed to ping the database: %v\n", err)
-		return
+		printErr("Failed to ping the database: %v\n", err)
 	}
 
 	if len(os.Args) > 2 {
@@ -102,27 +103,69 @@ func main() {
 		case "add-ip":
 			ip := os.Args[3]
 			data := os.Args[4]
-
 			err := insertNameData(db, "ips", ip, data)
 			if err != nil {
-				log.Fatalf("Failed to insert IP: %v", err)
+				printErr("Failed to insert: %v", err)
 			}
 		case "add-token":
 			token := os.Args[3]
 			data := os.Args[4]
-
 			err := insertNameData(db, "tokens", token, data)
 			if err != nil {
-				log.Fatalf("Failed to insert IP: %v", err)
+				printErr("Failed to insert: %v", err)
+			}
+
+		case "del-ip":
+			ip := os.Args[3]
+			err := delName(db, "ips", ip)
+			if err != nil {
+				printErr("Failed to del: %v", err)
+			}
+
+		case "del-token":
+			token := os.Args[3]
+			err := delName(db, "tokens", token)
+			if err != nil {
+				printErr("Failed to del: %v", err)
+			}
+
+		case "del-proc":
+			proc := os.Args[3]
+			n, err := strconv.Atoi(proc)
+			if err != nil {
+				printErr("Error converting rowid to integer: %v", err)
+			}
+
+			del := deleteId(db, "procs", n)
+			if del != nil {
+				printErr("Failed to del: %v", del)
+			}
+
+		case "purge-procs":
+			err := truncateTable(db, "procs")
+			if err != nil {
+				printErr("Failed to purge: %v", err)
+			}
+
+		case "purge-ips":
+			err := truncateTable(db, "ips")
+			if err != nil {
+				printErr("Failed to purge: %v", err)
+			}
+
+		case "purge-tokens":
+			err := truncateTable(db, "tokens")
+			if err != nil {
+				printErr("Failed to purge: %v", err)
 			}
 
 		default:
-			fmt.Println("Invalid argument: ", os.Args[2])
+			printOut("Invalid argument: " + os.Args[2])
 		}
 		return
 	}
 
-	fmt.Println(`{"sqlite3":true}`)
+	printOut(`{"sqlite3":true}`)
 
 	//fmt.Println(usage())
 	//dbFile := os.Args[1]
@@ -130,6 +173,15 @@ func main() {
 	//listNewIPIds(db)
 	//listNewIPTms(db)
 	//listNewIPDb(db)
+}
+
+func printErr(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format+"\n", a...)
+	os.Exit(1)
+}
+
+func printOut(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stdout, format+"\n", a...)
 }
 
 func sqlite3Version(db *sql.DB) (string, error) {
@@ -157,10 +209,6 @@ func getSqlite3Version(dbFile string) (string, error) {
 
 	return version, nil
 }
-
-//func listTables(db *sql.DB) {
-//	fmt.Println("listTables")
-//}
 
 func listTables(db *sql.DB) {
 	// Query all table names in the database from sqlite_master
@@ -252,166 +300,6 @@ func listNewIPDb(db *sql.DB) {
 //---
 //---
 
-//Failed to mark IP as processed: database is locked
-/*
-func listNewIPDb(db *sql.DB) {
-	lastID := getLastProcessedIDDb(db)
-	//rows, err := db.Query("SELECT id, ip_address FROM ips WHERE id > ?", lastID)
-	rows, err := db.Query("SELECT rowid, ip FROM ips WHERE rowid > ?", lastID)
-	if err != nil {
-		log.Fatalf("Failed to query new IPs: %v", err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var id int
-		var ipAddress string
-		if err := rows.Scan(&id, &ipAddress); err != nil {
-			log.Printf("Failed to scan row: %v", err)
-			continue
-		}
-		fmt.Printf("New IP: %s\n", ipAddress)
-		markIPAsProcessed(db, id)
-	}
-
-	// Check for errors from iterating over rows.
-	if err := rows.Err(); err != nil {
-		log.Fatalf("Failed during rows iteration: %v", err)
-	}
-}
-
-//----
-
-const cacheFileName = "last_processed_id.txt"
-
-func getLastProcessedTimestamp() time.Time {
-	content, err := ioutil.ReadFile(cacheFileName)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return time.Time{}
-		}
-		log.Fatalf("Failed to read the cache file: %v", err)
-	}
-
-	parsedTime, err := time.Parse(time.RFC3339, string(content))
-	if err != nil {
-		log.Fatalf("Failed to parse timestamp from cache: %v", err)
-	}
-
-	return parsedTime
-}
-
-func updateCacheFile(timestamp time.Time) {
-	timeStr := timestamp.Format(time.RFC3339)
-	err := ioutil.WriteFile(cacheFileName, []byte(timeStr), 0644)
-	if err != nil {
-		log.Fatalf("Failed to update cache file: %v", err)
-	}
-}
-
-func listNewIPTms(db *sql.DB) {
-	lastTimestamp := getLastProcessedTimestamp()
-	rows, err := db.Query("SELECT Ip, Timestamp FROM ips WHERE Timestamp > ?", lastTimestamp)
-	if err != nil {
-		log.Fatalf("Failed to query new IPs: %v", err)
-	}
-	defer rows.Close()
-
-	var latestTimestamp time.Time = lastTimestamp
-	for rows.Next() {
-		var ip string
-		var timestamp string
-
-		if err := rows.Scan(&ip, &timestamp); err != nil {
-			log.Printf("Failed to scan row: %v", err)
-			continue
-		}
-
-		//rowTime, err := time.Parse("2006-01-02 15:04:05", timestamp)
-		rowTime, err := time.Parse(time.RFC3339, timestamp)
-		if err != nil {
-			log.Printf("Failed to parse timestamp from row: %v", err)
-			continue
-		}
-
-		if rowTime.After(latestTimestamp) {
-			latestTimestamp = rowTime
-		}
-
-		fmt.Printf("New IP: %s (Timestamp: %s)\n", ip, timestamp)
-	}
-
-	// Update cache file with the latest timestamp
-	if latestTimestamp.After(lastTimestamp) {
-		updateCacheFile(latestTimestamp)
-	}
-
-	// Check for errors from iterating over rows.
-	if err := rows.Err(); err != nil {
-		log.Fatalf("Failed during rows iteration: %v", err)
-	}
-}
-
-// -------
-
-func getLastProcessedIDFile() int {
-	content, err := ioutil.ReadFile(cacheFileName)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return 0
-		}
-		log.Fatalf("Failed to read the cache file: %v", err)
-	}
-	lastID, err := strconv.Atoi(string(content))
-	if err != nil {
-		log.Fatalf("Failed to convert content to integer: %v", err)
-	}
-	return lastID
-}
-
-func updateCacheFileId(id int) {
-	err := ioutil.WriteFile(cacheFileName, []byte(strconv.Itoa(id)), 0644)
-	if err != nil {
-		log.Fatalf("Failed to update cache file: %v", err)
-	}
-}
-
-func listNewIPIds(db *sql.DB) {
-	lastID := getLastProcessedIDFile()
-	rows, err := db.Query("SELECT rowid, ip FROM ips WHERE rowid > ?", lastID)
-	if err != nil {
-		log.Fatalf("Failed to query new IPs: %v", err)
-	}
-	defer rows.Close()
-
-	var highestID int = lastID
-	for rows.Next() {
-		var id int
-		var ipAddress string
-		if err := rows.Scan(&id, &ipAddress); err != nil {
-			log.Printf("Failed to scan row: %v", err)
-			continue
-		}
-		if id > highestID {
-			highestID = id
-		}
-		fmt.Printf("New IP: %s\n", ipAddress)
-	}
-
-	// Update cache file with the highest ID
-	if highestID > lastID {
-		updateCacheFileId(highestID)
-	}
-
-	// Check for errors from iterating over rows.
-	if err := rows.Err(); err != nil {
-		log.Fatalf("Failed during rows iteration: %v", err)
-	}
-}
-
-// -------
-*/
-
 func listTokens(db *sql.DB) {
 	rows, err := db.Query("SELECT Name, Data, Timestamp FROM tokens")
 	if err != nil {
@@ -496,14 +384,25 @@ func insertNameData(db *sql.DB, table string, name string, data string) error {
 	return nil
 }
 
-func insertIpData(db *sql.DB, table string, ip string, data string) error {
+func delName(db *sql.DB, table string, name string) error {
 
-	query := `INSERT INTO ` + table + ` (ip,data) VALUES (?,?)`
-	_, err := db.Exec(query, ip, data)
+	query := `DELETE FROM ` + table + ` WHERE name = ?`
+	result, err := db.Exec(query, name)
 	if err != nil {
-		return fmt.Errorf("failed to insert IP into %s: %v", table, err)
+		return fmt.Errorf("failed to delete name from %s: %v", table, err)
 	}
 
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("failed to get affected rows: %v", err)
+	}
+
+	if rowsAffected == 0 {
+		//return "No rows were deleted."
+		return fmt.Errorf("No rows were deleted.")
+	}
+
+	//return fmt.Sprintf("success")
 	return nil
 }
 
@@ -544,6 +443,79 @@ func getTableRows(db *sql.DB, table string) ([]map[string]interface{}, error) {
 	}
 
 	return result, nil
+}
+
+func getRowId(db *sql.DB, table string, rowid int) ([]string, []interface{}, error) {
+	// Prepare the query statement
+	query := "SELECT rowid,* FROM " + table + " WHERE rowid = ?"
+	rows, err := db.Query(query, rowid)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer rows.Close()
+
+	// Get the column names
+	columnNames, err := rows.Columns()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Prepare a slice of interface{} to hold the values
+	values := make([]interface{}, len(columnNames))
+	scanArgs := make([]interface{}, len(columnNames))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	// Iterate through the rows and scan the values
+	if rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			return nil, nil, err
+		}
+	} else {
+		return nil, nil, sql.ErrNoRows
+	}
+
+	return columnNames, values, nil
+}
+
+func deleteId(db *sql.DB, table string, rowid int) error {
+
+	query, err := db.Exec("DELETE FROM "+table+" WHERE rowid = ?", rowid)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := query.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return errors.New("delete failed")
+	}
+
+	return err
+}
+
+func truncateTable(db *sql.DB, table string) error {
+
+	query, err := db.Exec("DELETE FROM " + table)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := query.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return errors.New("truncate failed")
+	}
+
+	return err
 }
 
 /*
