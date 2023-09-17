3d71927b69020cb7c2d3c9e8f1d54518e4d9fbcc
Karl Rink <karl@rink.us>
2023-09-17 09:45:47 -0700

    🍁 2023-09-17 1

---
+++ ubeken/crypto/fernet/golang/ed/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"time"
+
+	"github.com/fernet/fernet-go"
+)
+
+func main() {
+
+	key := "12345678901234567890123456789012"
+
+	// Encode the key as a base64 string
+	base64Key := base64.StdEncoding.EncodeToString([]byte(key))
+	fmt.Println("base64 key: " + base64Key)
+	//k := fernet.MustDecodeKeys("cw_0x689RpI-jtRR7oE8h_eQsKImvJapLeSbXpwF4e4=")
+	k := fernet.MustDecodeKeys(base64Key)
+
+	tok, err := fernet.EncryptAndSign([]byte("hello"), k[0])
+	if err != nil {
+		panic(err)
+	}
+
+	//fmt.Println(tok)
+	base64Tok := base64.StdEncoding.EncodeToString([]byte(tok))
+	fmt.Println("base64 Encrypted: " + base64Tok)
+
+	msg := fernet.VerifyAndDecrypt(tok, 60*time.Second, k)
+	fmt.Println(string(msg))
+
+}
