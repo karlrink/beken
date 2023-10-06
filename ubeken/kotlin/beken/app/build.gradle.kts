e63a57cd90f490248111e9af4638b8d168841f77
Karl Rink <karl@rink.us>
2023-10-06 15:23:07 -0700

    1.0-v4 kotlin android

--- ubeken/kotlin/beken/app/build.gradle.kts
+++ ubeken/kotlin/beken/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "us.rink.beken"
         minSdk = 24
         targetSdk = 33
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
